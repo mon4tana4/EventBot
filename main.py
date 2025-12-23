f56ca9b0fcc330ee00583a5185ab954206d7a849
Artur Kabisov <kabisovartur6@gmail.com>
2025-12-24 01:34:13 +0300

    added imports

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 """Лабораторная работа номер 4"""
+
+import os
+import json
+import asyncio
+from datetime import datetime, date, timedelta
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
