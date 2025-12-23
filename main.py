47117616075b1e974ba58d70e09bd8c3dd16fbc3
Artur Kabisov <kabisovartur6@gmail.com>
2025-12-24 01:38:03 +0300

    added my token

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 
 BOT_TOKEN = "8320022661:AAHEf6qV60tVXSJ3fDi7KhpviMU2cUM3ihM"
+
+DATA_DIR = "data"
+os.makedirs(DATA_DIR, exist_ok=True)
+EVENTS_FILE = os.path.join(DATA_DIR, "events.json")
+
+storage = MemoryStorage()
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(storage=storage)
+
+class EventStates(StatesGroup):
+    waiting_title = State()
+    waiting_description = State()
+    waiting_date = State()
+    waiting_time = State()
